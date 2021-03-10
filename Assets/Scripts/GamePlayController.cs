7a19bbb2b7b5060dfef620b336114a6dbdc808f7
BlazerX88 <65087254+BlazerX88@users.noreply.github.com>
2021-03-10 15:06:25 +0530

    Files

---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayController : MonoBehaviour
+{
+
+	public void BackToMainMenu()
+	{
+		Application.LoadLevel("MainMenu");
+	}
+
+}
